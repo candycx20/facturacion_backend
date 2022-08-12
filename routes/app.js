55efd855521f1164ba33fd893a38655edeca043a
titomon98 <arturomonterroso3@gmail.com>
2022-08-12 17:08:06 -0600

    Agregando archivos de configuracion

---
+++ routes/app.js
@@ -0,0 +1,34 @@
+
+const express       = require('express');
+const cors          = require('cors');
+const logger        = require('morgan');
+const http = require('http');
+const app = express();
+
+app.use(logger('dev'));
+
+//validacion de rutas
+app.use(cors());
+
+/* Agruegué el de express que el de bodyparser daba deprecate */
+app.use(express.json({limit:"50mb"}));  
+app.use(express.urlencoded({limit:"50mb" , extended: false }));  
+
+
+require("./routes/app")(app);
+
+app.use(express.static('./public'));
+
+
+app.get('*', (req, res) => res.status(200).send({
+     message: 'Index.',
+}));
+
+
+
+
+const port = parseInt(process.env.PORT, 10) || 3000;
+app.set('port', port);
+const server = http.createServer(app);
+server.listen(port);
+module.exports = app;
