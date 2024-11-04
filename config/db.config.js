3d9d0b221244271980777128cae660410be160f9
candycx20 <candycarrillo060@gmail.com>
2024-11-04 12:51:06 -0600

    Configuracion bd

--- config/db.config.js
+++ config/db.config.js
@@ -1,11 +1,11 @@
 
 module.exports = {
-    HOST: "localhost",
-    USER: "root",
-    PASSWORD: "",
-    DB: "barberia",
+    HOST: "database-1.c9s08qs0yb7v.us-east-2.rds.amazonaws.com",
+    USER: "admin",
+    PASSWORD: "Q3HH7DQ6",
+    DB: "facturacion",
     dialect: "mysql",
-    port: 3307,
+    port: 3308,
     pool: {
       max: 5,
       min: 0,
